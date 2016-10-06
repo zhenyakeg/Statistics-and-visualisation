1ebb66cfd34acf8479054cf2628dcb2b70140f93
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-10-06 18:57:03 +0300

    1

--- Hystogram.py
+++ Hystogram.py
@@ -1,1 +1,9 @@
 __author__ = 'student'
+import random
+import matplotlib.pyplot as plt
+
+random.seed(0)
+n = 9000000
+values = [random.normalvariate(0, 1) for i in range(n)]
+plt.hist(values, bins=10000)
+plt.show()
