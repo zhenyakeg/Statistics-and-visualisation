a642ae3385db5ee86a75690e217b2d3426ea0b83
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-10-06 21:15:59 +0300

    1

--- Equalisation.py
+++ Equalisation.py
@@ -1,5 +1,7 @@
 __author__ = 'student'
 import numpy as np
+import matplotlib.pyplot as plt
+import random
 def get_percentile(values, bucket_number):
     P=[0.0]
     for i in range (1, bucket_number):
@@ -11,7 +13,37 @@ def get_percentile_number(value,percentiles):
         if x<value:
             k=percentiles.index(x)
     return k
-def value_equalisation(value,  percentiles):
-    idx =get_percentile_number(value, percentiles)
-    new_value  = idx*1/len(percentiles)
+def value_equalisation(value,  percentiles, add_random=True):
+    random_noise = random.uniform(0,5/len(percentiles))
+    idx = get_percentile_number(value, percentiles)
+    if add_random:
+        new_value  = idx*1/len(percentiles)+random_noise
+    else:
+        new_value = idx*1/len(percentiles)
     return new_value
+
+
+data_file=open('img.txt', 'r')
+data=[list(map(float, l.split()))  for l in data_file.readlines()]
+
+fdata = []
+for d in data:
+    fdata.extend(d)
+
+plt.hist(fdata, bins=150)
+plt.show()
+p = get_percentile(fdata, 100)
+
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        data[i][j] = value_equalisation(data[i][j], p)
+
+fdata1 = []
+for d in data:
+    fdata1.extend(d)
+plt.hist(fdata1, bins=150)
+plt.show()
+
+plt.imshow(data, cmap = plt.get_cmap('gray'))
+plt.show()
+
